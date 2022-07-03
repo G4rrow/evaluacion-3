9e2c8fc69c6c2c8001ea333e23643728f44ab328
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2022-07-03 00:29:51 -0400

    mejorar admin

--- PyP/admin.py
+++ PyP/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from .models import Categoria, Producto
+from django.utils.safestring import mark_safe
 # Register your models here.
 
-admin.site.register(
-    Categoria
-)
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display=('idCategoria','nombreCategoria')
 
-admin.site.register(
-    Producto
-)
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display=('idProducto','nombreProducto','imgProduc','precio','favorito','categoria')
+
+    def imgProduc(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url = obj.imgProducto.url,
+            width=50,
+            height=50,
+            )
+    )
